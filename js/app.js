291803d0ac53e1edfbf84e600e197364ebe9bc60
Giovanne8708 <Giovannesiqueira87@gmail.com>
2026-07-31 11:32:28 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ import { initDB } from './storage.js';
 import { initRouter } from './router.js';
 import { initDashboard } from './dashboard.js';
 import { initAlunos } from './alunos.js';
-// Não importe Livros
 
 document.addEventListener('DOMContentLoaded', () => {
     if (!sessionStorage.getItem('auth_token')) {
